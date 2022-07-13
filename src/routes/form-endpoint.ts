78d5f0a647559e7c74a6765dde882af20ac4c409
f-elix <guerinfelix08@gmail.com>
2022-07-13 10:45:49 -0400

    Add try catch

--- src/routes/form-endpoint.ts
+++ src/routes/form-endpoint.ts
@@ -1,12 +1,20 @@
 import type { RequestHandler } from './__types/form-endpoint';
 
 export const post: RequestHandler = async ({ request }) => {
-	const body = await request.formData();
-	console.log(body.get('anything'));
-	return {
-		status: 303,
-		headers: {
-			location: request.headers.get('referer') || '/'
-		}
-	};
+	try {
+		const body = await request.formData();
+		console.log(body.get('anything'));
+		return {
+			status: 303,
+			headers: {
+				location: request.headers.get('referer') || '/'
+			}
+		};
+	} catch (error) {
+		console.error(error);
+		return {
+			status: 500,
+			body: error instanceof Error ? error.message : 'Server error'
+		};
+	}
 };
